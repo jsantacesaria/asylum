b9f04a82c41bdd59042ec874e0e55c2119903a39
EC2 Default User <ec2-user@ip-172-31-16-162.ec2.internal>
2020-03-08 19:03:09 +0000

    Beginning of contact form validation

--- app/Contact.php
+++ app/Contact.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
-    //
+   protected $fillable = [
+      "contact_name", "email", "comments"
+   ]; 
 }
--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Contact;
 
 class ContactsController extends Controller
 {
@@ -16,13 +17,20 @@ class ContactsController extends Controller
    }
 
     public function store() {
-
+/*
      $contact = new \App\Contact();
      $contact->contact_name = request('contact_name');
      $contact->email = request('email');
      $contact->comments = request('comments');
 
      $contact->save();
+*/
+
+     Contact::create([
+       'contact_name' => request('contact_name'),
+       'email' => request('email'),
+       'comments' => request('comments')
+     ]);
 
      return redirect('thanks');
    }
